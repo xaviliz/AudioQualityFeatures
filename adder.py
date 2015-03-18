d1e21ef38f997ce99bcb1cf1eafde856a37de2c1
xaviliz <xavilizarraga@gmail.com>
2015-03-19 00:06:39 +0100

    come on

--- adder.py
+++ adder.py
@@ -39,7 +39,8 @@ play(filename2)
 """
 namespace1 = 'lowlevelcd'
 namespace2 = 'lowlevelvy'
-
+#plot condition
+pcont = 0
 # spectral analysis parameters
 hopSize = 128
 frameSize = 2048
@@ -90,7 +91,7 @@ energybandratiolf = EnergyBandRatio(sampleRate = 44100, startFrequency= 0, stopF
 for frame in FrameGenerator(audio1, frameSize = 2048, hopSize = 128):
     frame = dcremoval(frame)
     pool.add('lowlevelcd.logattacktime', logattacktime(frame))
-    pool.add('lowlevelcd.spec.logattacktime', logattacktime(spectrum(w(frame))))
+    pool.add('lowlevelcd.spec_logattacktime', logattacktime(spectrum(w(frame))))
     pool.add('lowlevelcd.centroid', (sampleRate/2.)*(centroid(spectrum(derivative(w(frame))))))
     pool.add('lowlevelcd.hfc', hfc(spectrum(w(frame))))
     pool.add('lowlevelcd.lfenergy', energy(lowp(w(frame))))
@@ -109,7 +110,7 @@ for frame in FrameGenerator(audio1, frameSize = 2048, hopSize = 128):
 for frame in FrameGenerator(audio2, frameSize = 2048, hopSize = 128):
     frame = dcremoval(frame)
     pool.add('lowlevelvy.logattacktime', logattacktime(frame))
-    pool.add('lowlevelvy.spec.logattacktime', logattacktime(spectrum(w(frame))))
+    pool.add('lowlevelvy.spec_logattacktime', logattacktime(spectrum(w(frame))))
     pool.add('lowlevelvy.centroid', (sampleRate/2.)*(centroid(spectrum(derivative(w(frame))))))
     pool.add('lowlevelvy.hfc', hfc(spectrum(w(frame))))
     pool.add('lowlevelvy.lfenergy', energy(lowp(w(frame))))
@@ -129,206 +130,207 @@ nframes = float(len(audio1))/hopSize
 t1 = np.arange(0,float(len(audio1))/sampleRate,float(hopSize)/sampleRate)
 t2 = np.arange(0,float(len(audio2))/sampleRate,float(hopSize)/sampleRate)
 
-# Drawing LLD  - HFC
-plt.figure(1, figsize=(11.5, 9))
-plt.subplot(4,1,1)
-plt.plot(np.arange(audio1.size)/float(sampleRate), audio1, 'b')
-plt.axis([0, audio1.size/float(sampleRate), min(audio1), max(audio1)])
-xlabel('Time(s)'), plt.ylabel('amplitude')
-plt.title('x (cd version.wav)')
-
-plt.subplot(4,1,2)
-plt.plot(np.arange(audio2.size)/float(sampleRate), audio2, 'b')
-plt.axis([0, audio2.size/float(sampleRate), min(audio2), max(audio2)])
-plt.ylabel('amplitude')
-xlabel('Time(s)'),plt.title('x (vinyl version.wav)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,3)
-plot(t1,pool['lowlevelcd.hfc'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('High Frequency Content (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,4)
-plot(t2,pool['lowlevelvy.hfc'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('High Frequency Content (vinyl version)')
-plt.autoscale(tight=True)
-plt.show()
-
-# LF energy and HFenergy
-plt.figure(2, figsize=(11.5, 9))
-plt.subplot(4,1,1)
-plot(t1,pool['lowlevelcd.lfenergy'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('LFEnergy (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,2)
-plot(t2,pool['lowlevelvy.lfenergy'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('LFEnergy (vinyl version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,3)
-plot(t1,pool['lowlevelcd.hfenergy'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('HFEnergy (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,4)
-plot(t2,pool['lowlevelvy.hfenergy'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('HFEnergy (vinyl version)')
-plt.autoscale(tight=True)
-plt.show()
-
-# log attack time - Crest Factor (Time domain)
-plt.figure(3, figsize=(11.5, 9))
-plt.subplot(4,1,1)
-plot(pool['lowlevelcd.logattacktime'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('LogattackTime (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,2)
-plot(pool['lowlevelvy.logattacktime'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('LogAttackTime (vinyl version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,3)
-plot(t1,pool['lowlevelcd.crestfactor'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Crst Factor (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,4)
-plot(t2,pool['lowlevelvy.crestfactor'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Crest Factor (vinyl version)')
-plt.autoscale(tight=True)
-plt.show()
-
-# Spectral centroid of derivative - Derivative (Time domain)
-plt.figure(4, figsize=(11.5, 9))
-plt.subplot(4,1,1)
-plot(t1,pool['lowlevelcd.centroid'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Spectral dCentroid (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,2)
-plot(t2,pool['lowlevelvy.centroid'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Spectral dCentroid (vinyl version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,3)
-plot(t1,pool['lowlevelcd.derivative'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Derivative (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,4)
-plot(t2,pool['lowlevelvy.derivative'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Derivative (vinyl version)')
-plt.autoscale(tight=True)
-plt.show()
-
-# Low Frequency band ratio - High Frequency band ratio
-plt.figure(5, figsize=(11.5, 9))
-plt.subplot(4,1,1)
-plot(t1,pool['lowlevelcd.lfbandratio'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('LF Band Ratio (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,2)
-plot(t2,pool['lowlevelvy.lfbandratio'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('LF Band Ratio (vinyl version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,3)
-plot(t1,pool['lowlevelcd.hfbandratio'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('HF Band Ratio (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,4)
-plot(t2,pool['lowlevelvy.hfbandratio'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('HF Band Ratio (vinyl version)')
-plt.autoscale(tight=True)
-plt.show()
-
-# Rolloff (Time Domain) - Flatness SFX
-plt.figure(6, figsize=(11.5, 9))
-plt.subplot(4,1,1)
-plot(t1,pool['lowlevelcd.rolloff'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Roll Off (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,2)
-plot(t2,pool['lowlevelvy.rolloff'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Roll Off (vinyl version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,3)
-plot(t1,pool['lowlevelcd.flatnesssfx'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Flatness SFX (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,4)
-plot(t2,pool['lowlevelvy.flatnesssfx'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Flatness SFX (vinyl version)')
-plt.autoscale(tight=True)
-plt.show()
-
-# Strong Decay (Frequency Domain) -Entropy
-plt.figure(7, figsize=(11.5, 9))
-plt.subplot(4,1,1)
-plot(t1,pool['lowlevelcd.strongdecay'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Strong Decay (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,2)
-plot(t2,pool['lowlevelvy.strongdecay'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Strong Decay (vinyl version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,3)
-plot(t1,pool['lowlevelcd.entropy'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Entropy (cd version)')
-plt.autoscale(tight=True)
-
-plt.subplot(4,1,4)
-plot(t2,pool['lowlevelvy.entropy'].T[1:])
-axis('tight'), xlabel('Time(s)')
-title('Entropy (vinyl version)')
-plt.autoscale(tight=True)
-plt.show()
-
-# Histogram for cd version and vynil version
-figure(8)
-plt.subplot(2,1,1)
-plt.hist(audio1,50), xlim([-1,1]),title('cd version')
-plt.subplot(2,1,2)
-plt.hist(audio2,50), xlim([-1,1]),title('vinyl version')
+if (pcond != 0):
+    # Drawing LLD  - HFC
+    plt.figure(1, figsize=(11.5, 9))
+    plt.subplot(4,1,1)
+    plt.plot(np.arange(audio1.size)/float(sampleRate), audio1, 'b')
+    plt.axis([0, audio1.size/float(sampleRate), min(audio1), max(audio1)])
+    xlabel('Time(s)'), plt.ylabel('amplitude')
+    plt.title('x (cd version.wav)')
+    
+    plt.subplot(4,1,2)
+    plt.plot(np.arange(audio2.size)/float(sampleRate), audio2, 'b')
+    plt.axis([0, audio2.size/float(sampleRate), min(audio2), max(audio2)])
+    plt.ylabel('amplitude')
+    xlabel('Time(s)'),plt.title('x (vinyl version.wav)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,3)
+    plot(t1,pool['lowlevelcd.hfc'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('High Frequency Content (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,4)
+    plot(t2,pool['lowlevelvy.hfc'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('High Frequency Content (vinyl version)')
+    plt.autoscale(tight=True)
+    plt.show()
+    
+    # LF energy and HFenergy
+    plt.figure(2, figsize=(11.5, 9))
+    plt.subplot(4,1,1)
+    plot(t1,pool['lowlevelcd.lfenergy'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('LFEnergy (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,2)
+    plot(t2,pool['lowlevelvy.lfenergy'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('LFEnergy (vinyl version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,3)
+    plot(t1,pool['lowlevelcd.hfenergy'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('HFEnergy (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,4)
+    plot(t2,pool['lowlevelvy.hfenergy'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('HFEnergy (vinyl version)')
+    plt.autoscale(tight=True)
+    plt.show()
+    
+    # log attack time - Crest Factor (Time domain)
+    plt.figure(3, figsize=(11.5, 9))
+    plt.subplot(4,1,1)
+    plot(pool['lowlevelcd.logattacktime'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('LogattackTime (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,2)
+    plot(pool['lowlevelvy.logattacktime'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('LogAttackTime (vinyl version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,3)
+    plot(t1,pool['lowlevelcd.crestfactor'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Crst Factor (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,4)
+    plot(t2,pool['lowlevelvy.crestfactor'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Crest Factor (vinyl version)')
+    plt.autoscale(tight=True)
+    plt.show()
+    
+    # Spectral centroid of derivative - Derivative (Time domain)
+    plt.figure(4, figsize=(11.5, 9))
+    plt.subplot(4,1,1)
+    plot(t1,pool['lowlevelcd.centroid'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Spectral dCentroid (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,2)
+    plot(t2,pool['lowlevelvy.centroid'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Spectral dCentroid (vinyl version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,3)
+    plot(t1,pool['lowlevelcd.derivative'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Derivative (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,4)
+    plot(t2,pool['lowlevelvy.derivative'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Derivative (vinyl version)')
+    plt.autoscale(tight=True)
+    plt.show()
+    
+    # Low Frequency band ratio - High Frequency band ratio
+    plt.figure(5, figsize=(11.5, 9))
+    plt.subplot(4,1,1)
+    plot(t1,pool['lowlevelcd.lfbandratio'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('LF Band Ratio (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,2)
+    plot(t2,pool['lowlevelvy.lfbandratio'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('LF Band Ratio (vinyl version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,3)
+    plot(t1,pool['lowlevelcd.hfbandratio'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('HF Band Ratio (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,4)
+    plot(t2,pool['lowlevelvy.hfbandratio'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('HF Band Ratio (vinyl version)')
+    plt.autoscale(tight=True)
+    plt.show()
+    
+    # Rolloff (Time Domain) - Flatness SFX
+    plt.figure(6, figsize=(11.5, 9))
+    plt.subplot(4,1,1)
+    plot(t1,pool['lowlevelcd.rolloff'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Roll Off (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,2)
+    plot(t2,pool['lowlevelvy.rolloff'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Roll Off (vinyl version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,3)
+    plot(t1,pool['lowlevelcd.flatnesssfx'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Flatness SFX (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,4)
+    plot(t2,pool['lowlevelvy.flatnesssfx'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Flatness SFX (vinyl version)')
+    plt.autoscale(tight=True)
+    plt.show()
+    
+    # Strong Decay (Frequency Domain) -Entropy
+    plt.figure(7, figsize=(11.5, 9))
+    plt.subplot(4,1,1)
+    plot(t1,pool['lowlevelcd.strongdecay'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Strong Decay (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,2)
+    plot(t2,pool['lowlevelvy.strongdecay'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Strong Decay (vinyl version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,3)
+    plot(t1,pool['lowlevelcd.entropy'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Entropy (cd version)')
+    plt.autoscale(tight=True)
+    
+    plt.subplot(4,1,4)
+    plot(t2,pool['lowlevelvy.entropy'].T[1:])
+    axis('tight'), xlabel('Time(s)')
+    title('Entropy (vinyl version)')
+    plt.autoscale(tight=True)
+    plt.show()
+    
+    # Histogram for cd version and vynil version
+    figure(8)
+    plt.subplot(2,1,1)
+    plt.hist(audio1,50), xlim([-1,1]),title('cd version')
+    plt.subplot(2,1,2)
+    plt.hist(audio2,50), xlim([-1,1]),title('vinyl version')
 
 # Computing the kurtosis or skewness. CD version looks a normal 
 # distribution whereas vinyl version is a skewed normal distribution with a short deviation or IQR.
-stats = ['mean','var','min','max','median','dmean','dvar' ]
+stats = ['mean','var','min','max','median','dmean','dmean2','dvar','dvar','dvar2' ]
 
 megalopool = PoolAggregator(defaultStats=stats)(pool)
 # save to output file
